3551a8577c4c47ed87b77f2795583121d151ac3d
ozenesat <ozenesat@gmail.com>
2020-10-01 17:13:18 -0400

    Updates

---
+++ precache-manifest.a9bd81f391ffc7bdeb6bd46a8a58f377.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d0f8a39ce09719b16e0c1ffbdbe51f",
+    "url": "/reward-points/index.html"
+  },
+  {
+    "revision": "5d47fda5a2f5f3793183",
+    "url": "/reward-points/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a75b9789ec2aafe6f6ba",
+    "url": "/reward-points/static/css/main.01102b2d.chunk.css"
+  },
+  {
+    "revision": "5d47fda5a2f5f3793183",
+    "url": "/reward-points/static/js/2.b837d2ea.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reward-points/static/js/2.b837d2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75b9789ec2aafe6f6ba",
+    "url": "/reward-points/static/js/main.386073ae.chunk.js"
+  },
+  {
+    "revision": "3bc562c81fc426264a03",
+    "url": "/reward-points/static/js/runtime-main.628d2ffd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reward-points/static/media/logo.5d5d9eef.svg"
+  }
+]);
